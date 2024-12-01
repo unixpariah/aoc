3c3d997a18a35bcace88f0a60271f1f386990e78
unixpariah <oskar.rochowiak@tutanota.com>
2024-12-01 10:49:05 +0100

    first commit
    first commit

---
+++ 2024/days/01/code.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/jpillora/puzzler/harness/aoc"
+)
+
+func main() {
+	aoc.Harness(run)
+}
+
+func run(part2 bool, input string) any {
+	if part2 {
+		return "not implemented"
+	}
+	// solve part 1 here
+	return 42
+}
